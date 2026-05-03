c5d3721366f01945fff3d003e61dae566e748c6e
zhumazhanb <dedsec011@gmail.com>
2026-05-03 19:24:46 +0500

    force deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://smart-inventory-backend-a1yi.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000,
 });
 
